df13340a5641a873149c627fd3dd87eaada742b7
Wellington Barbosa <wellington_ueg@hotmail.com>
2022-07-24 19:43:35 -0300

    Projeto criado para a conclusão do bootcamp santander

---
+++ src/main/java/one/digitalinnovation/gof/model/EnderecoRepository.java
@@ -0,0 +1,15 @@
+ /**
+ * 
+ * @author Wellington Barbosa de Jesus
+ *
+ */
+ 
+ package one.digitalinnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
